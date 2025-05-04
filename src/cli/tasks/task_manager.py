0b62822335e5188a0637966101289d02404a34e4
BenGermider <bengermidr@gmail.com>
2025-05-04 23:56:05 +0300

    Fixed not working commands

--- src/cli/tasks/task_manager.py
+++ src/cli/tasks/task_manager.py
@@ -3,7 +3,6 @@ import threading
 from datetime import datetime
 
 from src.cli.command import Command
-from src.cli.utils.consts import JSON_DATABASE_PATH
 from src.cli.tasks.task import Task
 from src.cli.utils.paths import get_path
 
@@ -20,6 +19,7 @@ class TaskManager:
             "update": self.update_task,
             "delete": self.delete_task
         }
+        self.database = get_path("../..", "database", "json_database.json")
 
     def run(self, args) -> None:
         """
@@ -34,15 +34,14 @@ class TaskManager:
             self._mapping[new_command.command](new_command.args)
 
 
-    @staticmethod
-    def show_filter(task_filter=None):
+    def show_filter(self, task_filter=None):
         """
         Filtering user's tasks according to desired filter
         :param task_filter:
         :return:
         """
         try:
-            with open(JSON_DATABASE_PATH, "r") as f:
+            with open(self.database, "r") as f:
                 data = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             return
@@ -51,18 +50,19 @@ class TaskManager:
             data_to_show = [task for task in data["tasks"] if data["tasks"]["status"] == task_filter]
         else:
             data_to_show = data["tasks"]
+        print(data_to_show)
 
-    @staticmethod
-    def status_change(command: Command) -> None:
+    def status_change(self, command: Command) -> None:
         """
         Updating status of task
         :param command:
         :return:
         """
+        json_database_path = get_path()
         new_status = command.command.split("-", 1)[1]
         task_id, = command.args
         try:
-            with open(JSON_DATABASE_PATH, "r") as f:
+            with open(self.database, "r") as f:
                 data = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             return
@@ -72,21 +72,19 @@ class TaskManager:
                 tasks["status"] = new_status
                 tasks["updated_at"] = datetime.now().isoformat()
 
-        with open(JSON_DATABASE_PATH, "w") as f:
+        with open(self.database, "w") as f:
             json.dump(data, f, indent=4)
 
 
-    @staticmethod
-    def update_task(command: list) -> None:
+    def update_task(self, command: list) -> None:
         """
         Name change of task
         :param command:
         :return:
         """
         task_id, new_name = command
-        file_path = JSON_DATABASE_PATH
         try:
-            with open(file_path, "r") as file:
+            with open(self.database, "r") as file:
                 data = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             return
@@ -96,22 +94,20 @@ class TaskManager:
                 tasks["description"] = new_name
                 tasks["updated_at"] = datetime.now().isoformat()
 
-        with open(file_path, "w") as file:
+        with open(self.database, "w") as file:
             json.dump(data, file, indent=4)
 
 
-    @staticmethod
-    def add_task(task_desc: list[str]):
+    def add_task(self, task_desc: list[str]):
         """
         Add task to DB
         :param task_desc:
         :return:
         """
-        file_path = get_path("../..", "database", "json_database.json")
         task = Task(*task_desc)
 
         try:
-            with open(file_path, "r") as file:
+            with open(self.database, "r") as file:
                 data = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             data = {"tasks": []}
@@ -120,7 +116,7 @@ class TaskManager:
 
 
         try:
-            with open(file_path, "w") as file:
+            with open(self.database, "w") as file:
                 json.dump(data, file, indent=4)
         except FileNotFoundError:
             print("NO DATABASE")
@@ -129,24 +125,22 @@ class TaskManager:
         print(f"Task id {task.task_id} added successfully")
         return True
 
-    @staticmethod
-    def delete_task(task_id: int) -> bool:
+    def delete_task(self, task_id: int) -> bool:
         """
         Delete task from DB
         :param task_id:
         :return:
         """
         task_id, = task_id
-        file_path = JSON_DATABASE_PATH
         try:
-            with open(file_path, "r") as file:
+            with open(self.database, "r") as file:
                 data = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             return False
 
         data["tasks"] = [task for task in data["tasks"] if task["task_id"] != int(task_id)]
 
-        with open(file_path, "w") as file:
+        with open(self.database, "w") as file:
             json.dump(data, file, indent=4)
 
         return True
