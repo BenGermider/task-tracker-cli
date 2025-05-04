389c5240efa57450bd0864d5543c992a8abe9541
BenGermider <bengermidr@gmail.com>
2025-05-04 19:56:03 +0300

    Added list function

--- src/cli/tasks/task_manager.py
+++ src/cli/tasks/task_manager.py
@@ -1,5 +1,6 @@
 import json
 import threading
+from datetime import datetime
 
 from src.cli.command import Command
 from src.cli.structs.consts import JSON_DATABASE_PATH
@@ -13,11 +14,11 @@ class TaskManager:
         threading.Thread(target=self.run).start()
         self._mapping = {
             "add": self.add_task,
-            "list": None,
-            "mark-in-progress": None,
-            "mark-todo": None,
-            "mark-done": None,
-            "update": None,
+            "list": self.show_filter,
+            "mark-in-progress": self.status_change,
+            "mark-todo": self.status_change,
+            "mark-done": self.status_change,
+            "update": self.update_task,
             "delete": self.delete_task
         }
 
@@ -32,6 +33,44 @@ class TaskManager:
         self._running = False
 
     @staticmethod
+    def show_filter(task_filter=None):
+        try:
+            with open(JSON_DATABASE_PATH, "r") as f:
+                data = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return
+
+        if not task_filter:
+            data_to_show = [task for task in data["tasks"] if data["tasks"]["status"] == task_filter]
+        else:
+            data_to_show = data["tasks"]
+        print(data_to_show)
+
+
+    @staticmethod
+    def status_change(command: Command) -> None:
+        pass
+
+    @staticmethod
+    def update_task(command: list) -> None:
+        task_id, new_name = command
+        file_path = JSON_DATABASE_PATH
+        try:
+            with open(file_path, "r") as file:
+                data = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return
+
+        for tasks in data["tasks"]:
+            if tasks["task_id"] == int(task_id):
+                tasks["description"] = new_name
+                tasks["updated_at"] = datetime.now().isoformat()
+
+        with open(file_path, "w") as file:
+            json.dump(data, file, indent=4)
+
+
+    @staticmethod
     def add_task(task_desc: list[str]):
         file_path = JSON_DATABASE_PATH
         task = Task(*task_desc)
@@ -44,14 +83,19 @@ class TaskManager:
 
         data["tasks"].append(task.__dict__)
 
-        with open(file_path, "w") as file:
-            json.dump(data, file, indent=4)
+
+        try:
+            with open(file_path, "w") as file:
+                json.dump(data, file, indent=4)
+        except FileNotFoundError:
+            print("NO DATABASE")
+            return False
 
         print(f"Task id {task.task_id} added successfully")
         return True
 
     @staticmethod
-    def delete_task(task_id: int):
+    def delete_task(task_id: int) -> bool:
         task_id, = task_id
         file_path = JSON_DATABASE_PATH
         try:
@@ -64,5 +108,7 @@ class TaskManager:
 
         with open(file_path, "w") as file:
             json.dump(data, file, indent=4)
+
+        return True
 
 
